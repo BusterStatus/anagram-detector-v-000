fae21eea462952212208cbed90650a7beaf05aa2
matthew.serre <matthew.serre@gmail.com>
2018-11-08 03:06:24 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+class Anagram
+  
+  def initialize(word)
+    @word = word
+  end
+  
+  def match(anagrams)
+    word_array = @word.split
+    anagrams.each do |anagram|
+      if anagram.split.sort == word.array.sort
+        anagram
+      else
+        []
+      end
+    end
+  end
+end
